ef671ef6a7d9bcb0a43071316887e92ce064ca09
Fx Suarez <francoisxaviersuarez@gmail.com>
2020-06-03 23:04:59 +0200

    get micro device index

--- mic_config.py
+++ mic_config.py
@@ -2,4 +2,6 @@ import pyaudio
 
 py_audio = pyaudio.PyAudio()
 for ii in range(py_audio.get_device_count()):
-	print(py_audio.get_device_info_by_index(ii).get('name'))
+		device = py_audio.get_device_info_by_index(ii)
+		if 'USB' in device['name']
+				print('micro index is', ii)
