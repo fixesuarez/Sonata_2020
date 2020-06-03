d2015323673e559c2e5068c14df4d4ef2c9e84fa
Fx Suarez <francoisxaviersuarez@gmail.com>
2020-06-04 00:54:53 +0200

    simple test

--- sonata.py
+++ sonata.py
@@ -52,24 +52,21 @@ def rainbow_cycle(wait):
 
 while True:
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((255, 0, 0))
+    pixels.fill((255, 0, 0))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
-    pixels.fill((255, 0, 0, 0))
-    pixels.show()
+    # pixels.fill((255, 0, 0, 0))
     time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 255, 0))
+    pixels.fill((0, 255, 0))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 255, 0, 0))
-    pixels.show()
+    # pixels.fill((0, 255, 0, 0))
     time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    # pixels.fill((0, 0, 255))
+    pixels.fill((0, 0, 255))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 0, 255, 0))
-    pixels.show()
+    # pixels.fill((0, 0, 255, 0))
     time.sleep(1)
 
-    rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
+    # rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
