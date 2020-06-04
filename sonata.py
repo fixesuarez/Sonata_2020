7197ebce0714438cc5e8f3693300e222c73f6926
Fx Suarez <francoisxaviersuarez@gmail.com>
2020-06-04 19:26:25 +0200

    f

--- sonata.py
+++ sonata.py
@@ -17,7 +17,7 @@ num_pixels = 30
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.9, auto_write=True, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.9, auto_write=False, pixel_order=ORDER
 )
 
 
