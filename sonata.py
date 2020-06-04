1968ba8e718e933aca61bef881ffec7833c483b9
Fx Suarez <francoisxaviersuarez@gmail.com>
2020-06-04 19:42:25 +0200

    extract loader

--- sonata.py
+++ sonata.py
@@ -2,6 +2,7 @@ import time
 import board
 import neopixel
 
+from utilities import launch_loading_animation
 from note_listener import NoteListener
 
 
@@ -10,58 +11,22 @@ from note_listener import NoteListener
 pixel_pin = board.D18
 
 # The number of NeoPixels
-num_pixels = 30
+num_pixels = 15
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 ORDER = neopixel.GRB
 
-pixels = neopixel.NeoPixel(
+neopixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.9, auto_write=False, pixel_order=ORDER
 )
 
+launch_loading_animation(0.002, neopixels, num_pixels)
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
-
-
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(wait)
-
-
-for i in range(3):
-    rainbow_cycle(0.002)  # rainbow cycle with 1ms delay per step
-time.sleep(1)
-while pixels.brightness > 0.0:
-        pixels.brightness -= 0.01
-        pixels.show()
-        time.sleep(0.01)
 try:
     note_listener = NoteListener(neopixel, 80, 710)
 
 except KeyboardInterrupt:
-    neopixel.brightness = 0.
+    neopixels.brightness = 0.
+    neopixels.show()
 
--- utilities.py
+++ utilities.py
@@ -1,3 +1,43 @@
 import time
+import neopixel
 
 millis = lambda: int(round(time.time() * 1000))
+
+def launch_loading_animation(frame_delay, neopixels, count_leds):
+    for i in range(3):
+        rainbow_cycle(frame_delay, neopixels, count_leds)
+
+    while neopixels.brightness > 0.:
+            neopixels.brightness -= 0.01
+            neopixels.show()
+            time.sleep(0.005)
+
+
+def rainbow_cycle(frame_delay, neopixels, count_leds):
+    for j in range(255):
+        for i in range(count_leds):
+            pixel_index = (i * 256 // count_leds) + j
+            neopixels[i] = wheel(pixel_index & 255)
+        neopixels.show()
+        time.sleep(frame_delay)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
